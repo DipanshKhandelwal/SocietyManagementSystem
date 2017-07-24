23cbbd88b456fe2f842d9e01e880b0542f9f0ee0
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-25 04:30:00 +0530

    Added new class User

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/SignUpActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/SignUpActivity.java
@@ -147,4 +147,25 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
+
+    class User {
+        public String name;
+        public String email;
+
+        public User() {
+        }
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
 }
