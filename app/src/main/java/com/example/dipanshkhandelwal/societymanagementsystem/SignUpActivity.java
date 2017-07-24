59ea532419c6151c38c286115fe024bc14b84da9
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-25 04:13:09 +0530

    Added firebase database

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/SignUpActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/SignUpActivity.java
@@ -1,13 +1,38 @@
 package com.example.dipanshkhandelwal.societymanagementsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText inputEmail , inputPassword , inputName;
+    private Button btnSignUp ;
+    private TextView btnLogin , btnReset;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        auth = FirebaseAuth.getInstance();
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+        
     }
 }
