49e664bebfe3c6ab2a1029f2720ea2536330af39
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-25 03:03:22 +0530

    Set onclick for reset password text

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/LoginActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/LoginActivity.java
@@ -3,12 +3,17 @@ package com.example.dipanshkhandelwal.societymanagementsystem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,6 +27,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+
         setContentView(R.layout.activity_login);
 
         inputEmail = (EditText) findViewById(R.id.EtEmail);
@@ -30,5 +42,12 @@ public class LoginActivity extends AppCompatActivity {
         btnSignUp = (TextView) findViewById(R.id.TvSignUp);
         btnLogin = (Button) findViewById(R.id.bLogIn);
         btnReset = (TextView) findViewById(R.id.TvReset);
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this,"start activity to reset password", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
