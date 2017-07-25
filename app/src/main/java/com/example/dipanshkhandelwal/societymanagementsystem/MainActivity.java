28bfd6ea7cea7cdd036fdf02fd5cbc60f95577cc
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-25 06:03:36 +0530

    Added exit menu

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.dipanshkhandelwal.societymanagementsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
     }
 
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.sign_out_menu://signouts
+                auth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 return true;
 
             default: return super.onOptionsItemSelected(item);
