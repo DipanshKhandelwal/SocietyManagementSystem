3801f30c4d385ad40ae0c51a9f98f6f6301ef2cc
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-25 22:58:55 +0530

    Added click listener for floating button

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.dipanshkhandelwal.societymanagementsystem;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,12 +17,21 @@ import java.security.PublicKey;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
+    FloatingActionButton add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         auth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
+
+        add = (FloatingActionButton) findViewById(R.id.new_resident);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddResidentsActivity.class));
+            }
+        });
     }
 
     @Override
