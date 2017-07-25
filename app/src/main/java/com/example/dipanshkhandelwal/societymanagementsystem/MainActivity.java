63c0a6b02594a50a722c0c3df2ece564b64682f6
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-25 05:42:35 +0530

    Added menu to the activity

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.dipanshkhandelwal.societymanagementsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inf = getMenuInflater();
+        inf.inflate(R.menu.main_menu , menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sign_out_menu://signouts
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
+        }
     }
 }
