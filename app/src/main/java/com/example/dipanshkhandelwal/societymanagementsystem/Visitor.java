2434fe3d434ade0f526447747af961a878edeb8d
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-26 21:15:57 +0530

    Added getters for class Visitor

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/Visitor.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/Visitor.java
@@ -11,7 +11,39 @@ public class Visitor {
     String purpose_to_visit;
     String address_of_visitor;
     String guard_on_duty;
-    
+
     int in_time;
     int out_time;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDestinaion_address() {
+        return destinaion_address;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public String getPurpose_to_visit() {
+        return purpose_to_visit;
+    }
+
+    public String getAddress_of_visitor() {
+        return address_of_visitor;
+    }
+
+    public String getGuard_on_duty() {
+        return guard_on_duty;
+    }
+
+    public int getIn_time() {
+        return in_time;
+    }
+
+    public int getOut_time() {
+        return out_time;
+    }
 }
