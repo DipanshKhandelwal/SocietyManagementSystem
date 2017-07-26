2271ed0c80f62f06555af9d3501e0bbd646f12cf
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-26 21:15:13 +0530

    Added setters for class resident

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/Resident.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/Resident.java
@@ -13,29 +13,54 @@ public class Resident {
     String car_number ;
 
     int in_time;
-    int out_time;
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getAddress() {
         return address;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public int getPhone_number() {
         return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
     }
 
     public String getCar_number() {
         return car_number;
     }
 
+    public void setCar_number(String car_number) {
+        this.car_number = car_number;
+    }
+
     public int getIn_time() {
         return in_time;
+    }
+
+    public void setIn_time(int in_time) {
+        this.in_time = in_time;
     }
 
     public int getOut_time() {
         return out_time;
     }
+
+    public void setOut_time(int out_time) {
+        this.out_time = out_time;
+    }
+
+    int out_time;
 }
