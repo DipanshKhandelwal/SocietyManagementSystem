c552c507dfac1267fe019ba30b30a468d6addaae
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-27 23:30:41 +0530

    Added in AddResidentActivity

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/AddResidentsActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/AddResidentsActivity.java
@@ -1,13 +1,24 @@
 package com.example.dipanshkhandelwal.societymanagementsystem;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddResidentsActivity extends AppCompatActivity {
 
+    FloatingActionButton add ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_residents);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddResidentsActivity.this, AddResidentForm.class));
+            }
+        });
     }
 }
