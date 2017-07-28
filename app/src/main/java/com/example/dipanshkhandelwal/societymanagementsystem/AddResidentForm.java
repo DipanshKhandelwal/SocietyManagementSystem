3fd49f29fa05a85e76930bbda0af9ff8269db271
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-28 18:15:13 +0530

    Added createbutton click listener

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/AddResidentForm.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/AddResidentForm.java
@@ -2,10 +2,12 @@ package com.example.dipanshkhandelwal.societymanagementsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -37,5 +39,34 @@ public class AddResidentForm extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         create = (Button) findViewById(R.id.save);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String Sname = name.getText().toString().trim();
+                final String Saddress = address.getText().toString().trim();
+                final String Sphone = phone.getText().toString().trim();
+                final String Scar = car.getText().toString().trim();
+
+                if(TextUtils.isEmpty(Sname)){
+                    Toast.makeText(getApplicationContext(), "Enter name !" ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(Saddress)){
+                    Toast.makeText(getApplicationContext(), "Enter address !" ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(Sphone)){
+                    Toast.makeText(getApplicationContext(), "Enter phone number !" ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(Scar)){
+                    Toast.makeText(getApplicationContext(), "Enter car number !" ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
