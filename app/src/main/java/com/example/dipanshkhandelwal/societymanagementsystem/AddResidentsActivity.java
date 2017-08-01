fe4ac670ba5e196ebfdf085136e610d0027cfedd
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-08-02 01:23:46 +0530

    Added code to retrive residents and display it in layout

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/AddResidentsActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/AddResidentsActivity.java
@@ -5,14 +5,41 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class AddResidentsActivity extends AppCompatActivity {
 
     FloatingActionButton add ;
+    private FirebaseAuth auth;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+
+    private ResidentAdapter adapter;
+    private ArrayList<Resident> resident_info_list;
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_residents);
+
+        auth = FirebaseAuth.getInstance();
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("residents");
+
+        resident_info_list = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.resident_list);
+        adapter = new ResidentAdapter(this, R.layout.resident_layout, resident_info_list);
+        listView.setAdapter(adapter);
 
         add = (FloatingActionButton) findViewById(R.id.add_resident);
         add.setOnClickListener(new View.OnClickListener() {
@@ -21,5 +48,34 @@ public class AddResidentsActivity extends AppCompatActivity {
                 startActivity(new Intent(AddResidentsActivity.this, AddResidentForm.class));
             }
         });
+
+        mFirebaseDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                resident_info_list.add(dataSnapshot.getValue(Resident.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
+
