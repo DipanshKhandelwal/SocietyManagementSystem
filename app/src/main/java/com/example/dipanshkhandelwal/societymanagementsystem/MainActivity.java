c87b3d3c5c6ef6ecba7b91a24b2b5308f2003032
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-08-02 01:40:24 +0530

    Added new menu options

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.dipanshkhandelwal.societymanagementsystem;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,18 +13,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.security.PublicKey;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import static android.R.attr.entries;
+import static android.R.attr.x;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,8 +46,12 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private DatabaseReference mFirebaseDatabaseResidentList;
     private DatabaseReference mFirebaseDatabaseEntries;
+    private DatabaseReference mFirebaseDatabaseEntriesResident;
+    private DatabaseReference mFirebaseDatabaseEntriesVisitor;
     private FirebaseDatabase mFirebaseInstance;
     private List<Resident> Residents= new ArrayList<>();
+    private LinearLayout ResidentLayout, VisitorLayout;
+    private Boolean Resident;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +59,11 @@ public class MainActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
         mFirebaseInstance = FirebaseDatabase.getInstance();
         mFirebaseDatabaseResidentList = mFirebaseInstance.getReference("residents");
         mFirebaseDatabaseEntries = mFirebaseInstance.getReference("entries");
+        mFirebaseDatabaseEntriesResident = mFirebaseDatabaseEntries.child("residents");
+        mFirebaseDatabaseEntriesVisitor = mFirebaseDatabaseEntries.child("visitors");
 
         add = (FloatingActionButton) findViewById(R.id.new_resident);
         Allow = (ImageButton) findViewById(R.id.bAllow);
@@ -61,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
         TvOut = (TextView) findViewById(R.id.TvOut);
         TvIn = (TextView) findViewById(R.id.TvIn);
         TvDuration = (TextView) findViewById(R.id.TvDuration);
+
+        ResidentLayout = (LinearLayout) findViewById(R.id.ResidentLayout);
+        VisitorLayout = (LinearLayout) findViewById(R.id.VisitorLayout);
 
         name = (EditText) findViewById(R.id.FootOrCar);
 
@@ -83,16 +104,58 @@ public class MainActivity extends AppCompatActivity {
                 TvOut.setText("");
                 TvIn.setText("");
                 TvDuration.setText("");
+                name.setEnabled(true);
             }
         });
 
         Info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(checkForResident(name.getText().toString().trim())){
-                    updateResidentLayout();
+                Boolean isResident = false;
+                Resident resident = new Resident();
+                for(Resident x:Residents){
+                    if(name.getText().toString().trim().equals(x.name)){
+                        resident = x;
+                        isResident = true;
+                        break;
+                    }
+                }
+
+                name.setEnabled(false);
+
+                if(isResident){
+                    Toast.makeText(MainActivity.this,"Resident present in database",Toast.LENGTH_SHORT).show();
+                    Resident = true;
+                    updateResidentLayout(resident);
+                    Allow.setEnabled(true);
                 }else{
-                    updateVisitorLayout();
+                    Toast.makeText(MainActivity.this,"Resident isn't present in database",Toast.LENGTH_SHORT).show();
+                    Resident = false;
+                    //updateVisitorLayout();
+                    Allow.setEnabled(true);
+                }
+            }
+        });
+
+        Allow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Resident){
+
+                    Resident resident = new Resident(TvName.getText().toString(), TvAddress.getText().toString(), TvPhone.getText().toString(),TvCar.getText().toString(), TvOut.getText().toString(), TvIn.getText().toString());
+                    mFirebaseDatabaseEntries.child(name.getText().toString()).setValue(resident).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(MainActivity.this, "Resident Added successfully !!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "There was an error adding !!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }else{
+
                 }
             }
         });
@@ -125,6 +188,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void updateResidentLayout(final Resident resident) {
+        Query query = mFirebaseDatabaseEntries.child("entries").child(resident.getName()).orderByKey().limitToLast(1);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Resident resident2 = dataSnapshot.getValue(Resident.class);
+
+                if(resident2==null){
+                    Toast.makeText(MainActivity.this,"resident is null",Toast.LENGTH_SHORT).show();
+                    TvName.setText(resident.getName());
+                    TvAddress.setText(resident.getAddress());
+                    TvCar.setText(resident.getCar_number());
+                    TvPhone.setText(resident.getPhone_number());
+                    TvIn.setText("");
+                    String currentDateAndTime = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+                    TvOut.setText(currentDateAndTime);
+                }else{
+                    Toast.makeText(MainActivity.this,"resident is not null",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inf = getMenuInflater();
@@ -139,17 +230,15 @@ public class MainActivity extends AppCompatActivity {
                 auth.signOut();
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 return true;
+            case R.id.guard_info://signouts
+                startActivity(new Intent(MainActivity.this, GuardInfo.class));
+                return true;
+            case R.id.service_visitor_info://signouts
+                startActivity(new Intent(MainActivity.this, ServiceVisitorInfo.class));
+                return true;
 
             default: return super.onOptionsItemSelected(item);
         }
     }
 
-    private boolean checkForResident(String name){
-        for(Resident x:Residents){
-            if(name.equals(x.name)){
-                return true;
-            }
-        }
-        return false;
-    }
 }
