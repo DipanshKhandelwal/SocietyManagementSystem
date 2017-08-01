4730b1d26afee8c4560c43c64663901d6b1cc67b
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-08-02 03:17:29 +0530

    Added new empty activity ServiceVisitorInfo

---
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/ServiceVisitorInfo.java
@@ -0,0 +1,13 @@
+package com.example.dipanshkhandelwal.societymanagementsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceVisitorInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_visitor_info);
+    }
+}
