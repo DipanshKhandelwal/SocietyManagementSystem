285c3f74b99dad6ccffa34429553483d5ee308dc
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-08-01 14:34:58 +0530

    Added variables for Guard and added getters and setters

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/Guard.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/Guard.java
@@ -5,4 +5,40 @@ package com.example.dipanshkhandelwal.societymanagementsystem;
  */
 
 public class Guard {
+    String name;
+    String wroking_hours;
+    String phone_number;
+    String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWroking_hours() {
+        return wroking_hours;
+    }
+
+    public void setWroking_hours(String wroking_hours) {
+        this.wroking_hours = wroking_hours;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
