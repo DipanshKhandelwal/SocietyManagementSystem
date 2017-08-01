f35ddcf9cd297690d8ce591ebf3fcabe379a5f25
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-08-02 02:13:01 +0530

    Added GuardInfo.java class

--- app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/GuardInfo.java
+++ app/src/main/java/com/example/dipanshkhandelwal/societymanagementsystem/GuardInfo.java
@@ -1,13 +1,80 @@
 package com.example.dipanshkhandelwal.societymanagementsystem;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class GuardInfo extends AppCompatActivity {
+
+    FloatingActionButton add ;
+    private FirebaseAuth auth;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+
+    private GuardAdapter adapter;
+    private ArrayList<Guard> guard_info_list;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guard_info);
+
+        auth = FirebaseAuth.getInstance();
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("guards");
+
+        guard_info_list = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.guard_list);
+        adapter = new GuardAdapter(this, R.layout.guard_layout, guard_info_list);
+        listView.setAdapter(adapter);
+
+        add = (FloatingActionButton) findViewById(R.id.button);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GuardInfo.this, AddGuardForm.class));
+            }
+        });
+
+        mFirebaseDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                guard_info_list.add(dataSnapshot.getValue(Guard.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
